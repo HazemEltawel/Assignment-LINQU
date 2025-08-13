b0c0ba52094f3db2bd43eaa437ab7d9c5fe85957
HazemEltawel <eltawelhazem5@gmail.com>
2025-08-13 17:45:06 +0300

    Uses Count to get the number of odd numbers in the array

--- Experince-LINQU/Program.cs
+++ Experince-LINQU/Program.cs
@@ -1,5 +1,7 @@
 ﻿using Assignment_LINQU;
 using System.Linq;
+using System.Security.Cryptography;
+using static System.Runtime.InteropServices.JavaScript.JSType;
 
 class Program
 {
@@ -59,10 +61,23 @@ class Program
 
         #region Retrieve the second number greater than 5 
 
-        int[] numbers= { 5, 4, 1, 3, 9, 8, 6, 7, 2, 0 }; 
+        //int[] numbers= { 5, 4, 1, 3, 9, 8, 6, 7, 2, 0 }; 
 
-        var secondNumber=numbers.Where(n=> n > 5).Skip(1).FirstOrDefault();
-        Console.WriteLine(secondNumber);
+        //var secondNumber=numbers.Where(n=> n > 5).Skip(1).FirstOrDefault();
+        //Console.WriteLine(secondNumber);
+
+
+        #endregion
+
+        #region Uses Count to get the number of odd numbers in the array
+
+        int[] Arr = { 5, 4, 1, 3, 9, 8, 6, 7, 2, 0 };
+        var oodCount=(from n in Arr
+                      where n % 2 != 0
+                      select n).Count();
+
+        Console.WriteLine(oodCount);
+
 
 
         #endregion
