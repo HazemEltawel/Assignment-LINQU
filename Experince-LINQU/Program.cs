afa985e5bdbf346e5f4d7db351d1e8aecb9dc136
HazemEltawel <eltawelhazem5@gmail.com>
2025-08-13 17:06:06 +0300

    Add project files.

---
+++ Experince-LINQU/Program.cs
@@ -0,0 +1,19 @@
+﻿using Assignment_LINQU;
+using System.Linq;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        #region Find all products that are out of stock.
+        var outstock = from p in ListGenerators.ProductList
+                       where p.UnitsInStock == 0
+                       select p;
+        foreach (var item in outstock)
+        {
+            Console.WriteLine(item);
+        }
+        #endregion
+
+    }
+}
