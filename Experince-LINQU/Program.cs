145243ebe9b17019234d4ebfd99086ec9dac1421
HazemEltawel <eltawelhazem5@gmail.com>
2025-08-13 18:42:00 +0300

    Get the average length of the words in dictionary_english.txt (Read dictionary_english.txt into Array of String First).

--- Experince-LINQU/Program.cs
+++ Experince-LINQU/Program.cs
@@ -133,14 +133,22 @@ class Program
 
 
         #endregion
+
         #region Get the length of the longest word in dictionary_english.txt (Read dictionary_english.txt into Array of String First).
 
-        string[]Words= File.ReadAllLines("dictionary_english.txt");
-        var shortword=Words.Max(word=> word.Length);
-        Console.WriteLine($"Length of the shortest word: {shortword}");
+        //string[]Words= File.ReadAllLines("dictionary_english.txt");
+        //var shortword=Words.Max(word=> word.Length);
+        //Console.WriteLine($"Length of the shortest word: {shortword}");
 
 
         #endregion
+
+        #region Get the average length of the words in dictionary_english.txt (Read dictionary_english.txt into Array of String First).
+        string[]Words= File.ReadAllLines("dictionary_english.txt");
+        var shortword=Words.Average(word=> word.Length);
+        Console.WriteLine($"Length of the shortest word: {shortword}");
+        #endregion
+
 
 
 
