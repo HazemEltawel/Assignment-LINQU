cdfee2578ed00dadc8bca84ebe2141215f3ee0b5
HazemEltawel <eltawelhazem5@gmail.com>
2025-08-13 17:21:55 +0300

    Returns digits whose name is shorter than their

--- Experince-LINQU/Program.cs
+++ Experince-LINQU/Program.cs
@@ -14,16 +14,34 @@ class Program
         //    Console.WriteLine(item);
         //}
         #endregion
+
         #region Find all products that are in stock and cost more than 3.00 per unit.
 
-        var outstock= from p in ListGenerators.ProductList
-                      where p.UnitsInStock>0&&p.UnitPrice > 3.00m
-                      select p;
+        //var outstock= from p in ListGenerators.ProductList
+        //              where p.UnitsInStock>0&&p.UnitPrice > 3.00m
+        //              select p;
 
-        foreach (var item in outstock)
-         {
-             Console.WriteLine(item);
-         }
+        //foreach (var item in outstock)
+        // {
+        //     Console.WriteLine(item);
+        // }
         #endregion
+
+        #region  Returns digits whose name is shorter than their ==> value. String [] Arr = {"zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"};
+
+        String[] Arr = { "zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine" };
+
+        var shorterNames =
+        from digit in Arr.Select((name, Index) => new {Name = name ,Value = Index})
+        where digit.Name.Length < digit.Value
+        select digit;
+
+        foreach (var item in shorterNames)
+        {
+            Console.WriteLine(item);
+        }
+        #endregion
+
+
     }
 }
