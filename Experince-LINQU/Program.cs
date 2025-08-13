0bc754b8916e04064948d57eb7f76a4a32b25dfe
HazemEltawel <eltawelhazem5@gmail.com>
2025-08-13 17:07:09 +0300

    Find all products that are out of stock.

--- Experince-LINQU/Program.cs
+++ Experince-LINQU/Program.cs
@@ -5,7 +5,7 @@ class Program
 {
     static void Main(string[] args)
     {
-        #region Find all products that are out of stock.
+        #region Find all products that are out of stock. 
         var outstock = from p in ListGenerators.ProductList
                        where p.UnitsInStock == 0
                        select p;
